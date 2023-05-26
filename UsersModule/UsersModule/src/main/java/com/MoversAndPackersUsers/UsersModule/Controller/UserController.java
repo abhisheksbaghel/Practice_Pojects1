ea240fcde3365babcb10af96f88730de0a02bb57
Abhishek Singh Baghel <92367280+abhisheksbaghel@users.noreply.github.com>
2023-05-26 15:26:57 +0530

    user module commit

---
+++ UsersModule/UsersModule/src/main/java/com/MoversAndPackersUsers/UsersModule/Controller/UserController.java
@@ -0,0 +1,62 @@
+package com.MoversAndPackersUsers.UsersModule.Controller;
+
+import com.MoversAndPackersUsers.UsersModule.ExternalServices.DemoInterface;
+import com.MoversAndPackersUsers.UsersModule.Model.Users;
+import com.MoversAndPackersUsers.UsersModule.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userServ;
+
+    @PostMapping("/register")
+    public String add(@RequestBody Users user){
+
+        userServ.addUser(user);
+
+        return "Success";
+
+    }
+
+    @GetMapping("/getAllUsers")
+    public List<Users> getAll()
+    {
+        return userServ.selectAll();
+    }
+
+
+    @GetMapping("AllServiceProvider")
+    public ResponseEntity<?> getAllProvider()
+    {
+        return userServ.getAllProviders();
+    }
+
+    @Autowired
+    private DemoInterface demoInterface;
+
+    @GetMapping("/demo")
+    public String demo()
+    {
+        return demoInterface.demo();
+    }
+
+
+    @GetMapping("/getuserbyid/{email}")
+    public Users getById(@PathVariable String email)
+    {
+        return userServ.getByEmail(email);
+    }
+
+    @PostMapping("nearbyServices/{city}")
+    public ResponseEntity<?> getNearbyServices(@PathVariable String city)
+    {
+        return userServ.findNearby(city);
+    }
+}
